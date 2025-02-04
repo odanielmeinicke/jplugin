0c0d52aeb9f163498714b9e3e272c5df7d469ce2
Daniel Meinicke <dnlfg.contato@gmail.com>
2025-02-04 11:32:31 -0300

    added plugin info instance and moved classes

--- src/main/java/codes/laivy/plugin/info/PluginInfo.java
+++ src/main/java/codes/laivy/plugin/info/PluginInfo.java
@@ -1,7 +1,9 @@
-package codes.laivy.plugin;
+package codes.laivy.plugin.info;
 
+import codes.laivy.plugin.annotation.Initializer;
 import codes.laivy.plugin.exception.PluginInitializeException;
 import codes.laivy.plugin.exception.PluginInterruptException;
+import codes.laivy.plugin.loader.ConstructorPluginLoader;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -17,6 +19,14 @@ public interface PluginInfo {
     @NotNull Collection<@NotNull PluginInfo> getDependencies();
     @NotNull Collection<@NotNull PluginInfo> getDependants();
 
+    /**
+     * Represents the instance of this plugin, the instance of the reference. It could be
+     * null if the {@link Initializer} uses a PluginLoader that generates the instance like {@link ConstructorPluginLoader}
+     *
+     * @return
+     */
+    @Nullable Object getInstance();
+
     // Modules
 
     void start() throws PluginInitializeException;
@@ -27,12 +37,19 @@ public interface PluginInfo {
     enum State {
 
         IDLE,
+        FAILED,
+
         STARTING,
         RUNNING,
         STOPPING,
-
-        FAILED,
         ;
+
+        public boolean isRunning() {
+            return this == RUNNING;
+        }
+        public boolean isIdle() {
+            return this == IDLE || this == FAILED;
+        }
 
     }
 
