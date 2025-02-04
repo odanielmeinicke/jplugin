05837c342c3374f499f65c90355509a7056fedf8
Daniel Meinicke <dnlfg.contato@gmail.com>
2025-02-04 17:22:35 -0300

    improved PluginInfo classes

--- src/main/java/codes/laivy/plugin/info/PluginInfo.java
+++ src/main/java/codes/laivy/plugin/info/PluginInfo.java
@@ -1,23 +1,75 @@
 package codes.laivy.plugin.info;
 
+import codes.laivy.plugin.annotation.Category;
 import codes.laivy.plugin.annotation.Initializer;
+import codes.laivy.plugin.category.PluginHandler;
 import codes.laivy.plugin.exception.PluginInitializeException;
 import codes.laivy.plugin.exception.PluginInterruptException;
+import codes.laivy.plugin.factory.handlers.Handlers;
 import codes.laivy.plugin.loader.ConstructorPluginLoader;
+import codes.laivy.plugin.main.Plugins;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import org.jetbrains.annotations.Unmodifiable;
 
-import java.util.Collection;
+import java.util.*;
 
-public interface PluginInfo {
+public abstract class PluginInfo {
 
-    @Nullable String getName();
-    @NotNull State getState();
+    // Object
 
-    @NotNull Class<?> getReference();
+    private final @Nullable String name;
+    private final @NotNull Class<?> reference;
 
-    @NotNull Collection<@NotNull PluginInfo> getDependencies();
-    @NotNull Collection<@NotNull PluginInfo> getDependants();
+    private final @NotNull PluginInfo @NotNull [] dependencies;
+    public final @NotNull Set<@NotNull PluginInfo> dependants = new LinkedHashSet<>();
+
+    private volatile @NotNull State state = State.IDLE;
+    protected @Nullable Object instance;
+
+    private final @NotNull Handlers handlers = Handlers.create();
+
+    public PluginInfo(@NotNull Class<?> reference, @Nullable String name, @NotNull PluginInfo @NotNull [] dependencies) {
+        this.reference = reference;
+        this.name = name;
+        this.dependencies = dependencies;
+    }
+
+    // Getters
+
+    public @NotNull String getName() {
+        return name != null ? name : getReference().getName();
+    }
+
+    public final @NotNull State getState() {
+        return state;
+    }
+    protected void setState(@NotNull State state) {
+        @NotNull State previous = this.state;
+        this.state = state;
+
+        handle("change state", (handler) -> handler.state(this, previous));
+
+        if (state == State.RUNNING) {
+            handle("mark as running", (handler) -> handler.run(this));
+        }
+    }
+
+    public final @NotNull Class<?> getReference() {
+        return reference;
+    }
+
+    @Unmodifiable
+    public @NotNull Collection<@NotNull PluginInfo> getDependencies() {
+        return Arrays.asList(dependencies);
+    }
+    public final @NotNull Collection<@NotNull PluginInfo> getDependants() {
+        return dependants;
+    }
+
+    public @NotNull Handlers getHandlers() {
+        return handlers;
+    }
 
     /**
      * Represents the instance of this plugin, the instance of the reference. It could be
@@ -25,16 +77,60 @@ public interface PluginInfo {
      *
      * @return
      */
-    @Nullable Object getInstance();
+    public final @Nullable Object getInstance() {
+        return instance;
+    }
 
     // Modules
 
-    void start() throws PluginInitializeException;
-    void close() throws PluginInterruptException;
+    public void start() throws PluginInitializeException {
+        setState(State.STARTING);
+        handle("start", (handler) -> handler.start(this));
+    }
+    public void close() throws PluginInterruptException {
+        if (getState().isRunning()) {
+            return;
+        }
+
+        // Dependencies
+        @NotNull PluginInfo[] dependants = getDependants().stream().filter(dependency -> dependency.getState() != State.IDLE && dependency.getState() != State.FAILED).toArray(PluginInfo[]::new);
+
+        if (dependants.length > 0) {
+            @NotNull String list = Arrays.toString(dependants);
+            list = list.substring(1, list.length() - 1);
+
+            throw new PluginInterruptException(reference, "cannot interrupt plugin '" + getName() + "' because there's active dependants: " + list);
+        }
+
+        // Mark as stopping
+        setState(State.STOPPING);
+        handle("close", (handler) -> handler.close(this));
+
+        // The implementation should do the rest
+    }
+
+    // Implementations
+
+    @Override
+    public final boolean equals(@Nullable Object object) {
+        if (this == object) return true;
+        if (!(object instanceof PluginInfo)) return false;
+        @NotNull PluginInfo that = (PluginInfo) object;
+        return Objects.equals(getReference(), that.getReference());
+    }
+    @Override
+    public final int hashCode() {
+        return Objects.hashCode(getReference());
+    }
+
+    @Override
+    public final @NotNull String toString() {
+        return getName();
+    }
 
     // Classes
 
-    enum State {
+    public enum State {
 
         IDLE,
         FAILED,
@@ -51,6 +147,48 @@ public interface PluginInfo {
             return this == IDLE || this == FAILED;
         }
 
+    }
+
+    // Utilities
+
+    private void handle(@NotNull String action, @NotNull ThrowingConsumer<PluginHandler> consumer) {
+        // Call handlers
+        {
+            // Plugin handlers
+            for (@NotNull PluginHandler handler : getHandlers()) {
+                try {
+                    consumer.accept(handler);
+                } catch (@NotNull Throwable throwable) {
+                    throw new RuntimeException("cannot invoke plugin's handler to " + action + " '" + getName() + "': " + handler);
+                }
+            }
+
+            // Category handlers
+            for (@NotNull Category category : getReference().getAnnotationsByType(Category.class)) {
+                @NotNull String name = category.name();
+
+                for (@NotNull PluginHandler handler : Plugins.getFactory().getHandlers(name)) {
+                    try {
+                        consumer.accept(handler);
+                    } catch (@NotNull Throwable throwable) {
+                        throw new RuntimeException("cannot invoke category's handler to " + action + " '" + getName() + "': " + handler);
+                    }
+                }
+            }
+
+            // Global handlers
+            for (@NotNull PluginHandler handler : Plugins.getFactory().getHandlers()) {
+                try {
+                    consumer.accept(handler);
+                } catch (@NotNull Throwable throwable) {
+                    throw new RuntimeException("cannot invoke global handler to " + action + " '" + getName() + "': " + handler);
+                }
+            }
+        }
+    }
+    @FunctionalInterface
+    public interface ThrowingConsumer<T> {
+        void accept(T t) throws Throwable;
     }
 
 }
--- src/main/java/codes/laivy/plugin/loader/MethodPluginLoader.java
+++ src/main/java/codes/laivy/plugin/loader/MethodPluginLoader.java
@@ -5,14 +5,12 @@ import codes.laivy.plugin.exception.PluginInterruptException;
 import codes.laivy.plugin.info.PluginInfo;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import org.jetbrains.annotations.Unmodifiable;
 
 import java.io.Closeable;
 import java.io.Flushable;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.*;
 
 public final class MethodPluginLoader implements PluginLoader {
 
@@ -30,91 +28,28 @@ public final class MethodPluginLoader implements PluginLoader {
 
     // Classes
 
-    private static final class PluginInfoImpl implements PluginInfo {
+    private static final class PluginInfoImpl extends PluginInfo {
 
         // Object
 
-        private final @Nullable String name;
-        private final @NotNull Class<?> reference;
-
-        private final @NotNull PluginInfo @NotNull [] dependencies;
-        public final @NotNull Set<@NotNull PluginInfo> dependants = new LinkedHashSet<>();
-
-        private volatile @NotNull State state = State.IDLE;
-        private @Nullable Object instance;
-
         public PluginInfoImpl(@NotNull Class<?> reference, @Nullable String name, @NotNull PluginInfo @NotNull [] dependencies) {
-            this.reference = reference;
-            this.name = name;
-            this.dependencies = dependencies;
-        }
-
-        // Getters
-
-        public @NotNull Class<?> getReference() {
-            return reference;
-        }
-
-        public @NotNull String getName() {
-            return name != null ? name : getReference().getName();
-        }
-
-        @Override
-        @Unmodifiable
-        public @NotNull Collection<@NotNull PluginInfo> getDependencies() {
-            return Arrays.asList(dependencies);
-        }
-        @Override
-        public @NotNull Collection<@NotNull PluginInfo> getDependants() {
-            return dependants;
-        }
-
-        @Override
-        public @Nullable Object getInstance() {
-            return instance;
-        }
-
-        public @NotNull State getState() {
-            return state;
-        }
-
-        public boolean isRunning() {
-            return getState() == State.RUNNING;
-        }
-
-        // Implementations
-
-        @Override
-        public boolean equals(@Nullable Object object) {
-            if (this == object) return true;
-            if (!(object instanceof PluginInfoImpl)) return false;
-            @NotNull PluginInfoImpl that = (PluginInfoImpl) object;
-            return Objects.equals(getReference(), that.getReference());
-        }
-        @Override
-        public int hashCode() {
-            return Objects.hashCode(getReference());
-        }
-
-        @Override
-        public @NotNull String toString() {
-            return getName();
+            super(reference, name, dependencies);
         }
 
         // Modules
 
         @Override
         public void start() throws PluginInitializeException {
-            state = State.STARTING;
-
             try {
+                super.start();
+
                 @NotNull Method method = getReference().getDeclaredMethod("initialize");
                 method.setAccessible(true);
 
                 // Invoke method
                 this.instance = method.invoke(null);
             } catch (@NotNull Throwable throwable) {
-                state = State.FAILED;
+                setState(State.FAILED);
 
                 if (throwable instanceof InvocationTargetException) {
                     if (throwable.getCause() instanceof PluginInitializeException) {
@@ -131,28 +66,17 @@ public final class MethodPluginLoader implements PluginLoader {
             }
 
             // Mark as running
-            state = State.RUNNING;
+            setState(State.RUNNING);
         }
         @Override
         public void close() throws PluginInterruptException {
-            if (!isRunning()) {
+            if (!getState().isRunning()) {
                 return;
             }
 
-            // Dependencies
-            @NotNull PluginInfo[] dependants = getDependants().stream().filter(dependency -> dependency.getState() != State.IDLE && dependency.getState() != State.FAILED).toArray(PluginInfo[]::new);
-
-            if (dependants.length > 0) {
-                @NotNull String list = Arrays.toString(dependants);
-                list = list.substring(1, list.length() - 1);
-
-                throw new PluginInterruptException(reference, "cannot interrupt plugin '" + getName() + "' because there's active dependants: " + list);
-            }
-
-            // Mark as stopping
-            state = State.STOPPING;
-
             try {
+                super.close();
+
                 @NotNull Method method = getReference().getDeclaredMethod("interrupt");
                 method.setAccessible(true);
 
@@ -181,7 +105,7 @@ public final class MethodPluginLoader implements PluginLoader {
                 throw new PluginInterruptException(getReference(), "cannot access interrupt method", e);
             } finally {
                 instance = null;
-                state = State.IDLE;
+                setState(State.IDLE);
             }
         }
 
